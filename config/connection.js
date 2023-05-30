c05ee42063d386b8cb119f52c0ff5218cefd5175
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-30 15:56:08 -0500

    problem 2

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,3 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
-
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
@@ -36,14 +14,40 @@ if (process.env.JAWSDB_URL) {
     DB_USER='root',
     DB_PASSWORD='605291Lmtz$',
     {
-      // host: '127.0.0.1.',
-      host: 'localhost',
+      // host: 'localhost',
+            host: '127.0.0.1.',
       dialect: 'mysql',
-      dialectOption:{
-        decimalNumbers: true,
-      },
+      port: 3306
     }
   );
 }
 
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     // DB_NAME='ShrimpBlogs_db',
+//     // DB_USER='root',
+//     // DB_PASSWORD='605291Lmtz$',
+//     {
+//       host: '127.0.0.1.',
+//       // host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOption:{
+//         decimalNumbers: true,
+//       },
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
