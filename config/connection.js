5eff58464241ca2c33a9107a5134ae25566bbe39
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-29 20:14:07 -0500

    api routes are working

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,25 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
@@ -7,13 +29,18 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    DB_NAME='ShrimpBlogs_db',
+    DB_USER='root',
+    DB_PW='605291Lmtz$',
     {
-      host: 'localhost',
+      host: '127.0.0.1.',
       dialect: 'mysql',
-      port: 3306
+      dialectOption:{
+        decimalNumbers: true,
+      },
     }
   );
 }
