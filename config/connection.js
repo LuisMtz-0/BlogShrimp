ec737e6c3762dc92dffc6bd3ae94f3f8fe9104a2
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-30 16:36:13 -0500

    env working

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
-    DB_NAME='ShrimpBlogs_db',
-    DB_USER='root',
-    DB_PASSWORD='605291Lmtz$',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    // DB_NAME='ShrimpBlogs_db',
+    // DB_USER='root',
+    // DB_PASSWORD='605291Lmtz$',
     {
       // host: 'localhost',
             host: '127.0.0.1.',
