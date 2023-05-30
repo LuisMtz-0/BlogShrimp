71c821a6f10e86b7f08e316c6ab44510ac17860a
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-30 14:22:29 -0500

    connections fixed for heroku

--- config/connection.js
+++ config/connection.js
@@ -29,12 +29,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
-    DB_NAME='ShrimpBlogs_db',
-    DB_USER='root',
-    DB_PW='605291Lmtz$',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    // DB_NAME='ShrimpBlogs_db',
+    // DB_USER='root',
+    // DB_PW='605291Lmtz$',
     {
       host: '127.0.0.1.',
       dialect: 'mysql',
