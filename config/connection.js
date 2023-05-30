eeb3c9e1346cd867f52be57b0b6c098d3ed8a49c
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-30 16:45:10 -0500

    env files working

--- config/connection.js
+++ config/connection.js
@@ -21,30 +21,3 @@ if (process.env.JAWSDB_URL) {
 
 module.exports = sequelize;
 
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     // DB_NAME='ShrimpBlogs_db',
-//     // DB_USER='root',
-//     // DB_PASSWORD='605291Lmtz$',
-//     {
-//       host: '127.0.0.1.',
-//       // host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOption:{
-//         decimalNumbers: true,
-//       },
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
